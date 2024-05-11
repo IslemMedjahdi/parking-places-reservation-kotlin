9663f3f449737e82f5db7dd960455891d13f0277
IslemMedjahdi <ki_medjahdi@esi.dz>
2024-05-12 00:58:18 +0100

    add reserve view-model(not completed)

--- app/src/main/java/com/example/parking_places_reservation/view-models/RegisterViewModel.kt
+++ app/src/main/java/com/example/parking_places_reservation/view-models/RegisterViewModel.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.parking_places_reservation.core.retrofit.AuthRepository
 import com.example.parking_places_reservation.core.retrofit.Endpoint
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class RegisterViewModel(private val authRepository: AuthRepository): ViewModel() {
+class RegisterViewModel(private val authRepository: AuthRepository) : ViewModel() {
     var fullName = mutableStateOf("")
     var email = mutableStateOf("")
     var password = mutableStateOf("")
@@ -17,32 +19,33 @@ class RegisterViewModel(private val authRepository: AuthRepository): ViewModel()
     var success = mutableStateOf(false)
     var error = mutableStateOf("")
 
-    fun register(){
+    fun register() {
         loading.value = true
         success.value = false
         error.value = ""
         viewModelScope.launch {
-            val response = authRepository.register(
-                Endpoint.RegisterRequest(
-                    email = email.value,
-                    password = password.value,
-                    fullName = fullName.value
+            withContext(Dispatchers.IO) {
+                val response = authRepository.register(
+                    Endpoint.RegisterRequest(
+                        email = email.value,
+                        password = password.value,
+                        fullName = fullName.value
+                    )
                 )
-            )
-            if(response.isSuccessful){
-                success.value = true
-            }
-            else{
-                error.value = response.message()
-                if(error.value.isBlank()){
-                    error.value = response.code().toString()
+                if (response.isSuccessful) {
+                    success.value = true
+                } else {
+                    error.value = response.message()
+                    if (error.value.isBlank()) {
+                        error.value = response.code().toString()
+                    }
                 }
+                loading.value = false
             }
-            loading.value = false
         }
     }
 
-    class Factory(private val authRepository: AuthRepository): ViewModelProvider.Factory{
+    class Factory(private val authRepository: AuthRepository) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             return RegisterViewModel(authRepository) as T
         }
